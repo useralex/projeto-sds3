f0ea72e281f77938f32358a458a041613ca06ba3
Alexsandro Alves <alexsandro.alves@gmail.com>
2021-05-09 21:55:25 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -14,7 +14,6 @@ const DonutChart = () => {
     const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
     useEffect(() => {
-
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
             .then(response => {
                 const data = response.data as SaleSum[];
@@ -24,11 +23,6 @@ const DonutChart = () => {
                 setChartData({ labels: myLabels, series: mySeries });
             });
     }, []);
-
-    //const mockData = {
-    //    series: [477138, 499928, 444867, 220426, 473088],
-    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
 
     const options = {
         legend: {
